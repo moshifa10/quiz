72875c8f6ca796fe266a53353175d6daeb785e9e
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-02-11 20:48:26 +0200

    Now I am doing the ui

--- ui.py
+++ ui.py
@@ -1,2 +1,12 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Njabs Quiz")
+        self.score =  Label(text="Score", font=("Arial", 8, "normal"))
+
+        self.window.mainloop()
