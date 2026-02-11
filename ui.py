12c300c03493f3c7864c4498a9dc84152246f1b9
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-02-11 21:22:49 +0200

    added the ui now focused on making the functionality

--- ui.py
+++ ui.py
@@ -10,21 +10,19 @@ class QuizInterface:
         self.window.config(bg=THEME_COLOR, padx=50, pady=20)
 
         self.score =  Label(text="Score: 0", font=("Arial", 15, "normal") , bg=THEME_COLOR, fg="white")
-        self.score.grid(column=2, row=0)
+        self.score.grid(column=2, row=0,pady=20)
 
         self.canvas = Canvas(height=250,width=300,bg="white")
         self.canvas.create_text(150,125, text="Hello welcome", font=("Arial", 20, "italic"))
-        self.canvas.grid(column=0, row=1, columnspan=3)
+        self.canvas.grid(column=0, row=1, columnspan=4, pady= 20)
 
 
-        self.wrong = Canvas(height=100, width=100, bg=THEME_COLOR)
-        self.wrong_photo = PhotoImage(name="images/false.png")
-        self.wrong.create_image(50,50,image=self.wrong_photo)
-        self.wrong.grid(column=1, row=3)
+        self.wrong_photo = PhotoImage(file="images/false.png")
+        self.wrong = Button(image=self.wrong_photo, highlightthickness=0)
+        self.wrong.grid(column=1, row=3, pady=40)
 
 
-        self.right = Canvas(height=100, width=100,bg=THEME_COLOR)
-        self.right_photo = PhotoImage(name="images/true.png")
-        self.right.create_image(50,50,image=self.right_photo)
+        self.right_photo = PhotoImage(file="images/true.png")
+        self.right = Button(image=self.right_photo, highlightthickness=0)
         self.right.grid(column=2, row=3)
         self.window.mainloop()
